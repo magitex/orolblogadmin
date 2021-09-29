da28d4b14bca04d6fc4eeb98f70ebd4ef213a540
magitex <avik.rswebtech@gmail.com>
2021-09-28 23:27:26 -0700

    chng

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1d53f2e96d8f6cea3c49b7bb4a6811db'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2a3b8220-a96e-4a17-bb39-81a68c181e4b'
+};
